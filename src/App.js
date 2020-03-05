7068aab7599cfe0626907d227ff8c4c794ae5a84
airjordon2 <airjordon2@gmail.com>
2020-03-05 17:07:09 -0500

    Updating header styles

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>GAME TRIVIA!!!</h1>
+        <div className="game-trivia">
+          <h3 className="game">Game</h3>
+          <h1 className="trivia">Trivia!</h1>
+        </div>
       </header>
-      <hr />
+      <hr className="hr"/>
       <div className="game-wrapper">
         <TriviaGame />
       </div>
